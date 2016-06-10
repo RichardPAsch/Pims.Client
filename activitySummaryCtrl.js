7c52bbb719c7be37fd6a58325a12d24789baac8d
Richard P. Asch <rpasch@rpclassics.net>
2016-06-10 16:54:16 -0700

    Add files via upload

---
+++ activitySummaryCtrl.js
@@ -0,0 +1,43 @@
+﻿(function() {
+
+    "use strict";
+
+    angular
+        .module("incomeMgmt.activitySummary")
+        .controller("activitySummaryCtrl", activitySummaryCtrl);    
+
+    activitySummaryCtrl.$inject = ['activitySummarySvc'];
+
+
+
+    function activitySummaryCtrl(activitySummarySvc) {
+        var vm = this;
+        vm.currentYear = getCurrentYear();
+
+        activitySummarySvc.query(function (responseData) {
+            vm.incomeActivitySummaryData = responseData;
+            vm.incomeTotal = calculateTotal(responseData);
+
+        });
+     }
+
+
+    function getCurrentYear() {
+        var date = new Date();
+        return date.getFullYear();
+    }
+
+
+    function calculateTotal(dataToSum) {
+        var sum = 0;
+        for (var amt=0; amt < dataToSum.length; amt++) {
+            sum += dataToSum[amt].amountRecvd;
+        }
+        return sum;
+    }
+
+
+   
+    
+
+}());
---
+++ activitySummarySvc.js
@@ -0,0 +1,30 @@
+﻿(function(module) {
+
+    /*
+        Provides Income Received portion of 'Activity Summary' page..
+        Example WebApi json results for : http://localhost/Pims.Web.Api/api/Income/Averages
+        [
+            {"monthRecvd":1,"amountRecvd":37.17,"ytdAverage":37.17,"rolling3MonthAverage":0.0},
+            {"monthRecvd":3,"amountRecvd":33.19,"ytdAverage":35.18,"rolling3MonthAverage":0.0},
+            {"monthRecvd":4,"amountRecvd":131.22,"ytdAverage":67.19,"rolling3MonthAverage":67.19},
+            {"monthRecvd":5,"amountRecvd":389.21,"ytdAverage":147.70,"rolling3MonthAverage":184.54},
+            {"monthRecvd":6,"amountRecvd":42.98,"ytdAverage":126.75,"rolling3MonthAverage":187.80}
+        ]
+    */
+
+    "use strict";
+
+    angular
+        .module("incomeMgmt.core")
+        .factory("activitySummarySvc", incomeActivity);
+
+    incomeActivity.$inject = ["$resource", "appSettings"]; 
+
+
+    function incomeActivity($resource, appSettings) {
+        return $resource(appSettings.serverPath + "/Pims.Web.Api/api/Income/Averages");
+    };
+
+   
+
+}());
