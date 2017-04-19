8c5bb1d4c015ba38a9023dbde4aa7389f34852c4
Richard P. Asch <rpasch@rpclassics.net>
2017-04-18 20:22:28 -0700

    Add files via upload
    Initial creation - WIP to accommodate Position-transactions editing.

---
+++ transactionsModalCtrl.js
@@ -0,0 +1,44 @@
+﻿(function () {
+
+    "use strict";
+
+    /*
+        Reference: Plunker template testing via: http://plnkr.co/edit/E2x39J0uLIrdZeqXyC66
+    */
+
+    angular
+        .module("incomeMgmt.positionTransactions")
+        .controller("transactionsModalCtrl", transactionsModalCtrl);
+
+
+    transactionsModalCtrl.$inject = ['$scope', '$modal'];
+
+    function transactionsModalCtrl($scope, $modal) {
+
+        $scope.userRolesGrid = { enableRowSelection: true, enableRowHeaderSelection: false };
+
+        $scope.userRolesGrid.columnDefs = [
+                            { name: 'id' },
+                            { name: 'rolename' }
+        ];
+
+        $scope.gridTitle = "Position transaction(s)";
+
+        $scope.userRolesGrid.data = [
+            { id: 0, rolename: 'admin' },
+            { id: 1, rolename: 'superuser' },
+            { id: 2, rolename: 'username' }
+          ];
+
+      
+        $scope.openUserModal = function () {
+            $modal.open({
+                templateUrl: 'userModalContent.html',
+                size: 'dd',
+                scope: $scope
+            });
+        };
+
+    }
+
+}());
