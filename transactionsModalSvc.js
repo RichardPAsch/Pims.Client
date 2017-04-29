29eb368f33aa67670e5d8b512d2588176288a4a6
Richard P. Asch <rpasch@rpclassics.net>
2017-04-28 17:24:39 -0700

    Add files via upload
    Initial creation - continued WIP for transaction processing.

---
+++ transactionsModalSvc.js
@@ -0,0 +1,48 @@
+﻿(function () {
+
+   "use strict";
+
+    angular
+       .module("incomeMgmt.core")
+       .factory("transactionsModalSvc", transactionsModalSvc);
+
+    transactionsModalSvc.$inject = ["$resource", 'appSettings'];
+
+
+    function transactionsModalSvc($resource, appSettings) {
+
+        var vm = this;
+        vm.baseUrl = appSettings.serverPath + "/Pims.Web.Api/api/";
+
+
+
+        function calculateValuation(units, unitPrice) {
+            // Current market price used when conducting buy, sell, or rollover transactions.
+            return units * unitPrice;
+        }
+
+        function calculateCostBasis(valuation, fees) {
+            return valuation + fees;
+        }
+
+        function calculateUnitCost(netAmount, qty) {
+            return netAmount / qty;
+        }
+        
+
+
+
+
+
+        // API
+        return {
+            calculateValuation: calculateValuation,
+            calculateCostBasis: calculateCostBasis,
+            calculateUnitCost: calculateUnitCost
+        }
+
+    }
+
+
+
+}());
