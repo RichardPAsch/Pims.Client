3623a3c6de88534f5dc73a11dcb06689178d464e
Richard P. Asch <rpasch@rpclassics.net>
2017-07-17 17:01:16 -0700

    Add files via upload
    WIP - adding implementation for Transaction processing.

--- assetCreatePositionCtrl.js
+++ assetCreatePositionCtrl.js
@@ -14,20 +14,21 @@
 
         var vm = this;
         var positionCount = 0;
+        var today = new Date();
 
         vm.accountTypesCollection = accountTypes;
         vm.accountTypeSelected = "";
         vm.isOneOfManyPositions = false;
         // Fetch investors' entire Profile in case other properties are needed in the future.
         vm.currentInvestorProfile = currentInvestorSvc.profile;
+        // Fetch currently saved Asset Profile.
         vm.currentAsset = createAssetWizardSvc.processAsset();
         vm.showAAssetAcctTypeValidationMsg = false;
         vm.showQtyValidationMsg = false;
         vm.QuantityRegEx = "^[0-9.]+$";
         vm.inRevisitMode = false;
         vm.transactionFees = 0.00;
-        var today = new Date();
-  
+        vm.currentUnitMktPrice = this.currentAsset.ProfileToCreate.Price;
 
         if (vm.currentAsset.PositionsCreated != undefined) {
             // Always the most recently added Position will be available for REVISIT viewing.
@@ -39,18 +40,24 @@
             var idx = createAssetWizardSvc.getAccountTypeIndex(vm.currentAsset.PositionsCreated[positionCount].PostEditPositionAccount, accountTypes);
             vm.accountTypeSelected = vm.accountTypesCollection[idx];
             vm.assetTickerSymbol = vm.currentAsset.AssetTicker.toUpperCase();
-            vm.positionUnitPrice = vm.currentAsset.ProfileToCreate.Price;
+            vm.costBasis = vm.currentAsset.ProfileToCreate.Price;
             vm.inRevisitMode = true;
         } else {
             vm.assetTickerSymbol = vm.currentAsset.AssetTicker;
             vm.assetPurchaseDate = "";
             vm.positionQty = 0;
-            vm.positionUnitPrice = vm.currentAsset.ProfileToCreate == undefined ? 0 : vm.currentAsset.ProfileToCreate.Price;
-            vm.unitCosts = 0;
+            vm.costBasis = 0.00; 
+            vm.unitCosts = 0.00; 
             vm.positionLastUpdate = getLastUpdate();
             vm.referencedAcctType = createAssetWizardSvc.getBaseReferencingAccount();
             vm.accountTypeSelected = vm.accountTypesCollection[5]; // "Select..."
         } 
+
+
+        function getLastUpdate() {
+            var date = new Date();
+            return date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+        }
 
 
         vm.isMinimumPositionData = function () {
@@ -68,17 +75,34 @@
         vm.calculateCostBasis = function () {
             var unFormattedCostBasis = 0.0;
             
-            if (vm.transactionFees != "") {
+            if (vm.transactionFees > 0.00) {
                 if (incomeMgmtSvc.isValidCurrencyFormat(vm.transactionFees)) {
-                    unFormattedCostBasis = parseFloat(((vm.positionUnitPrice * vm.positionQty) + parseFloat(vm.transactionFees)));
+                    unFormattedCostBasis = parseFloat(((vm.currentUnitMktPrice * vm.positionQty) + parseFloat(vm.transactionFees)));
                 } else {
                     alert("Invalid fees entry.");
                 }
             } else {
-                    unFormattedCostBasis = parseFloat(((vm.positionUnitPrice * vm.positionQty)));
+                unFormattedCostBasis = parseFloat(((vm.currentUnitMktPrice * vm.positionQty)));
             }
 
-            vm.costBasis = createAssetWizardSvc.formatCurrency(unFormattedCostBasis.toString(), 2);
+            return unFormattedCostBasis.toFixed(2);
+        }
+
+
+        vm.calculateUnitCosts = function() {
+            return (vm.costBasis / vm.positionQty).toFixed(3);
+        }
+
+
+        vm.initializeCostBasisAndUnitCost = function() {
+            
+            if (vm.currentUnitMktPrice > 0.00 & vm.positionQty > 0) {
+                vm.costBasis = this.calculateCostBasis();
+                vm.unitCosts = this.calculateUnitCosts();
+            } else {
+                return 0.00;
+            }
+                
         }
 
 
@@ -94,13 +118,7 @@
                 ? false
                 : true;
         }
-
-
-        function getLastUpdate() {
-            var date = new Date();
-            return  date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
-        }
-                
+        
 
         vm.saveAssetPosition = function () {
 
@@ -143,12 +161,9 @@
             positionBuild.PostEditPositionAccount = vm.accountTypeSelected.accountTypeDesc;
             positionBuild.Qty = Number(vm.positionQty);
             positionBuild.ReferencedTickerSymbol = vm.currentAsset.AssetTicker.toUpperCase().trim();
-            positionBuild.CostBasis = createAssetWizardSvc.formatCurrency(vm.costBasis.toString(), 2);
-
-            positionBuild.UnitCost = incomeMgmtSvc.isValidCurrencyFormat(vm.positionUnitPrice.toString()) ? vm.positionUnitPrice : 0.0;
+            positionBuild.CostBasis = vm.costBasis;
+            positionBuild.UnitCost = vm.unitCosts;
             positionBuild.TransactionFees = vm.transactionFees;
-            
-            //positionBuild.UnitCost = createAssetWizardSvc.formatCurrency(vm.positionUnitPrice, 2);
             positionBuild.DateOfPurchase = $filter('date')(vm.assetPurchaseDate, 'M/dd/yyyy');
             positionBuild.LastUpdate = $filter('date')(vm.positionLastUpdate, 'M/dd/yyyy');
             positionBuild.DatePositionAdded = $filter('date')(today, 'M/dd/yyyy');
@@ -165,9 +180,21 @@
                                                                          + vm.currentAsset.AssetTicker.toUpperCase().trim()
                                                                          + "/Position/Account/"
                                                                          + vm.accountTypeSelected.accountTypeDesc; // use Guid instead ?
-            
+
+            // Initialize child Transaction object.
+            positionBuild.ReferencedTransaction.TransactionId = incomeMgmtSvc.createGrid();
+            positionBuild.ReferencedTransaction.TransactionEvent = "B";
+            positionBuild.ReferencedTransaction.Units = vm.positionQty;
+            positionBuild.ReferencedTransaction.MktPrice = vm.currentUnitMktPrice;
+            positionBuild.ReferencedTransaction.Fees = vm.transactionFees.toFixed(2);
+            positionBuild.ReferencedTransaction.UnitCost = vm.unitCosts;
+            positionBuild.ReferencedTransaction.CostBasis = vm.costBasis;
+            positionBuild.ReferencedTransaction.Valuation = (vm.positionQty * vm.currentUnitMktPrice).toFixed(2);
+            positionBuild.ReferencedTransaction.DateCreated = $filter('date')(today, 'M/dd/yyyy hh:mm');
+
             vm.currentAsset.PositionsCreated.push(positionBuild);
   
+
             // Update service with new Position(s).
             createAssetWizardSvc.processAsset(vm.currentAsset);
 
