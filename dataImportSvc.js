82bd59808920e5241afacf99db34c49536cc5d20
Richard P. Asch <rpasch@rpclassics.net>
2018-01-05 17:42:46 -0800

    Add files via upload
    Updated processImportFileModel().

--- dataImportSvc.js
+++ dataImportSvc.js
@@ -14,51 +14,27 @@
 
         var vm = this;
         vm.importFileControllerUrl = appSettings.serverPath + "/Pims.Web.Api/api/ImportFile";
-
         
+
         function processImportFileModel(importFileDataModel, ctrl) {
-            var test = 2;
-            // TODO: ok to this point. Double quotes on file path ok? 12.29.17
-            // TODO: sample file used: C:/Downloads/FidelityXLS/Portfolio_RevenueTEST_1_Fidelity.xlsx
 
-
-            $resource(vm.importFileControllerUrl).save(importFileDataModel.ImportFilePath.trim()).$promise.then(function () {
-                    ctrl.postAsyncProcessImportFile("ok");
+            $resource(vm.importFileControllerUrl).save(importFileDataModel).$promise.then(
+                function (responseMsg) {
+                    var respObj = responseMsg;
+                    ctrl.postAsyncProcessImportFile(responseMsg);
                 },
-                    function() {
-                        // to be implmented
-                    });
-
-
-            //var incomeUrl = appSettings.serverPath + "/Pims.Web.Api/api/Asset/" + incomeToSave.TickerSymbol + "/Income";
-
-            //if (!incomeMgmtSvc.isValidIncomeDateVsPositionAndTodayDate(incomeToSave.DateReceived, incomeToSave.PositionAddDate)) {
-            //    alert("Invalid entry; income date may not precede date 'Position' was created, \nnor exceed todays' date.");
-            //    return;
-            //}
-
-            //// Properties now unnecessary.
-            //delete incomeToSave.TickerSymbol;
-            //delete incomeToSave.PositionAddDate;
-
-
-            //$resource(incomeUrl).save(incomeToSave).$promise.then(function () {
-            //    ctrl.postAsyncSave(true);
-            //}, function () {
-            //    ctrl.postAsyncSave(false);
-            //});
-            return "ok";
+                function () {
+                    // error to be implemented.
+                }
+             );
         }
 
        
 
 
-        
-
         // API
         return {
             processImportFileModel: processImportFileModel
-           
         }
 
     }
