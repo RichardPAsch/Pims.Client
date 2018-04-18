690f45cbd252e46290897f5236ac5c0aaaef9f80
Richard P. Asch <rpasch@rpclassics.net>
2018-04-18 15:39:21 -0700

    Add files via upload
    Updated to accommodate asset type dropdowns.

--- queriesAssetSvc.js
+++ queriesAssetSvc.js
@@ -9,27 +9,48 @@
     queriesAssetSvc.$inject = ["$resource", "appSettings"];
 
     function queriesAssetSvc($resource, appSettings) {
-        //return $resource(appSettings.serverPath + "/Pims.Web.Api/api/Assets/:status");
+
         var vm = this;
         vm.assetSummaryData = [];
+        vm.assetTypes = [];
 
 
         function getAssetSummaryData(assetsWithPositionStatus, ctrl) {
+            // Fetch all current assets for logged investor.
+            // e.g.,assetsWithPositionStatus = (A)ctive
             var assetSummaryUrl = appSettings.serverPath + "/Pims.Web.Api/api/Assets/" + assetsWithPositionStatus;
 
             $resource(assetSummaryUrl).query().$promise.then(function (response) {
                 vm.assetSummaryData = response;
                 ctrl.postAsyncGetAssetSummaryData(vm.assetSummaryData);
             }, function (err) {
-                if (err.status == 400 || err.status == 500) {
+                if (err.status === 400 || err.status === 500) {
                     alert("Error retreiving portfolio asset summary data.");
                 }
             });
         }
        
 
+        function getAvailableAssetTypes(ctrl) {
+            // http://localhost//Pims.Web.Api/api/AssetClass
+            var assetTypesUrl = appSettings.serverPath + "/Pims.Web.Api/api/AssetClass";
+
+            $resource(assetTypesUrl).query().$promise.then(function (response) {
+                vm.assetTypes = response;
+                ctrl.postAsyncGetAvailableAssetTypes(vm.assetTypes);
+            }, function (err) {
+                if (err.status === 400 || err.status === 500) {
+                    alert("Error retreiving asset types data.");
+                }
+            });
+        }
+
+
+
+        // API
         return {
-            getAssetSummaryData: getAssetSummaryData
+            getAssetSummaryData: getAssetSummaryData,
+            getAvailableAssetTypes: getAvailableAssetTypes
         }
         
 
