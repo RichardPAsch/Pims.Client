4b34a8879f49935b27fc08ddc1ea50bf120b4b2e
Richard P. Asch <rpasch@rpclassics.net>
2018-05-29 15:13:48 -0700

    Add files via upload
    Updated syntax errors.

--- assetCreateProfileCtrl.js
+++ assetCreateProfileCtrl.js
@@ -31,13 +31,13 @@
             vm.assetPeRatio = vm.currentAsset.ProfileToCreate == undefined ? 0 : vm.currentAsset.ProfileToCreate.PE_Ratio;
             vm.assetEPS = vm.currentAsset.ProfileToCreate == undefined ? 0 : vm.currentAsset.ProfileToCreate.EarningsPerShare;
             vm.assetUnitPrice = vm.currentAsset.ProfileToCreate == undefined ? 0 : vm.currentAsset.ProfileToCreate.Price;
-            vm.assetDivPayDate = vm.currentAsset.ProfileToCreate == undefined ? "" : $filter('date')(vm.currentAsset.ProfileToCreate.DividendPayDate, 'MMM d, yyyy');
-            vm.assetExDivDate = vm.currentAsset.ProfileToCreate == undefined ? "" : $filter('date')(vm.currentAsset.ProfileToCreate.ExDividendDate, 'MMM d, yyyy');
+            vm.assetDivPayDate = vm.currentAsset.ProfileToCreate == undefined ? "" : $filter("date")(vm.currentAsset.ProfileToCreate.DividendPayDate, 'MMM d, yyyy');
+            vm.assetExDivDate = vm.currentAsset.ProfileToCreate == undefined ? "" : $filter("date")(vm.currentAsset.ProfileToCreate.ExDividendDate, 'MMM d, yyyy');
             vm.assetDescription = vm.currentAsset.ProfileToCreate == undefined ? "" : vm.currentAsset.ProfileToCreate.TickerDescription;
             if (vm.currentAsset.ProfileToCreate != undefined)
                 vm.showDateTime = true;
 
-            vm.refreshDateTime = vm.currentAsset.ProfileToCreate == undefined ? "" : $filter('date')(vm.currentAsset.ProfileToCreate.LastUpdate, 'MM/d/yyyy-hh:mm:ss a');
+            vm.refreshDateTime = vm.currentAsset.ProfileToCreate == undefined ? "" : $filter("date")(vm.currentAsset.ProfileToCreate.LastUpdate, 'MM/d/yyyy-hh:mm:ss a');
         } else {
             // Initialize view with fetched Profile data.
             vm.assetTickerSymbol = vm.currentAsset.AssetTicker; //.toUpperCase();
@@ -47,10 +47,10 @@
             vm.assetPeRatio = assetProfile.pE_Ratio == null ? 0 : assetProfile.pE_Ratio;
             vm.assetEPS = assetProfile.earningsPerShare;
             vm.assetUnitPrice = assetProfile.price;
-            vm.assetDivPayDate = assetProfile.dividendPayDate.indexOf("1900") == 0 ? "Not available" : $filter('date')(assetProfile.dividendPayDate, 'M/dd/yyyy');
-            vm.assetExDivDate = assetProfile.exDividendDate.indexOf("1900") == 0 ? "Not available" : $filter('date')(assetProfile.exDividendDate, 'M/dd/yyyy');
+            vm.assetDivPayDate = assetProfile.dividendPayDate.indexOf("1900") == 0 ? "Not available" : $filter("date")(assetProfile.dividendPayDate, 'M/dd/yyyy');
+            vm.assetExDivDate = assetProfile.exDividendDate.indexOf("1900") == 0 ? "Not available" : $filter("date")(assetProfile.exDividendDate, 'M/dd/yyyy');
             vm.assetDescription = assetProfile.tickerDescription;
-            vm.refreshDateTime = $filter('date')(new Date(), 'MM/d/yyyy-hh:mm:ss a');
+            vm.refreshDateTime = $filter("date")(new Date(), 'MM/d/yyyy-hh:mm:ss a');
             vm.showDateTime = true;
 
             toggleInputFields();
@@ -76,11 +76,12 @@
                
             var profileBuild = createAssetWizardSvc.getBaseProfile();
 
-            // Required 5 attributes:
+            // Required 6 attributes:
+            profileBuild.ProfileId = incomeMgmtSvc.createGuid();
             profileBuild.TickerSymbol = vm.assetTickerSymbol;
             profileBuild.TickerDescription = vm.assetDescription;
             profileBuild.Price = vm.assetUnitPrice;
-            profileBuild.LastUpdate = $filter('date')(new Date(vm.refreshDateTime), 'M/dd/yyyy');
+            profileBuild.LastUpdate = $filter("date")(new Date(vm.refreshDateTime), "M/dd/yyyy");
             profileBuild.Url = createAssetWizardSvc.getBasePath + "Profile/" + vm.assetTickerSymbol.trim();
 
             profileBuild.DividendRate = vm.assetDivRate == null ? 0 : vm.assetDivRate;
@@ -88,12 +89,12 @@
             profileBuild.DividendYield = vm.assetDivYield == null ? 0 : vm.assetDivYield;
             profileBuild.PE_Ratio = vm.assetPeRatio == null ? 0 : vm.assetPeRatio;
             profileBuild.EarningsPerShare = vm.assetEPS == null ? 0 : vm.assetEPS;
-            profileBuild.ExDividendDate = vm.assetExDivDate.trim() == "Not available"
-                ? $filter('date')(new Date("1/1/1900"), 'M/dd/yyyy')
-                : $filter('date')(new Date(vm.assetExDivDate), 'M/dd/yyyy');
-            profileBuild.DividendPayDate = vm.assetDivPayDate.trim() == "Not available"
-                ? $filter('date')(new Date("1/1/1900"), 'M/dd/yyyy')
-                : $filter('date')(new Date(vm.assetDivPayDate), 'M/dd/yyyy');
+            profileBuild.ExDividendDate = vm.assetExDivDate.trim() === "Not available"
+                ? $filter("date")(new Date("1/1/1900"), "M/dd/yyyy")
+                : $filter("date")(new Date(vm.assetExDivDate), "M/dd/yyyy");
+            profileBuild.DividendPayDate = vm.assetDivPayDate.trim() === "Not available"
+                ? $filter("date")(new Date("1/1/1900"), "M/dd/yyyy")
+                : $filter("date")(new Date(vm.assetDivPayDate), "M/dd/yyyy");
             
             var assetBuild = createAssetWizardSvc.processAsset();
             assetBuild.ProfileToCreate = profileBuild;
@@ -108,7 +109,7 @@
         vm.refreshProfile = function () {
             $state.go("asset_create.profile", { "tickerSymbol": vm.assetTickerSymbol.trim() });
             createAssetWizardSvc.showActiveTab('steps clearfix', 'profile');
-            vm.refreshDateTime = $filter('date')(new Date(), 'MM/d/yyyy-hh:mm:ss a');
+            vm.refreshDateTime = $filter("date")(new Date(), 'MM/d/yyyy-hh:mm:ss a');
             vm.lastRefresh = vm.refreshDateTime;
             vm.showDateTime = true;
         }
